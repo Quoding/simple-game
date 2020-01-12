c24e13e4c515c059e62fdd449fec6d0ded9bd2c2
Quoding <quoding@protonmail.ch>
2020-01-12 15:04:51 -0500

    Add .gitignore to repo

--- simple_game.py
+++ simple_game.py
@@ -10,14 +10,6 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 FPS = 7
 
-init_result = pygame.init()
-
-# Create a game window
-game_window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-
-# Set title
-pygame.display.set_caption("My simple game")
-
 # Set up
 game_running = True
 POS_X = 400
@@ -26,9 +18,16 @@ vel_x = 0
 vel_y = 0
 score = 0
 
+# Init pygame
+init_result = pygame.init()
+
+# Create a game window
+game_window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+# Set title
+pygame.display.set_caption("My simple game")
 clock = pygame.time.Clock()
 font = pygame.font.Font("freesansbold.ttf", 32)
-
 
 # Make the user controlled square spawn at pos_x, pos_y
 game_square = Square(POS_X, POS_Y)
@@ -81,48 +80,55 @@ while game_running:
         if event.type == pygame.KEYDOWN:
             # Process keydown events
             if event.key == pygame.K_w:
-                print("Move forwards")
+                # print("Move forwards")
                 game_square.vel_y = -25
                 game_square.vel_x = 0
             elif event.key == pygame.K_s:
-                print("Move backwards")
+                # print("Move backwards")
                 game_square.vel_y = 25
                 game_square.vel_x = 0
             elif event.key == pygame.K_a:
-                print("Move left")
+                # print("Move left")
                 game_square.vel_x = -25
                 game_square.vel_y = 0
             elif event.key == pygame.K_d:
-                print("Move right")
+                # print("Move right")
                 game_square.vel_x = 25
                 game_square.vel_y = 0
 
-    # Content here
+    # Make canvas white (i.e. reset the display to the current game state)
+    game_window.fill(WHITE)
+
+    # Handle velocity of the square
     game_square.pos_x += game_square.vel_x
     game_square.pos_y += game_square.vel_y
-    game_window.fill(WHITE)
+
+    # Check if player out of bounds
     if isOutOfBounds(game_square):
         game_running = False
         break
 
-    # preventOutOfBounds(game_square)
-    # print("Current position {} {}".format(game_square.pos_x, game_square.pos_y))
+    # Check if player acquired the objective square, if so, change objective square coordinates
     if gotObjective(game_square, objective_square):
         score += 1
-    # ---- Draw shapes here ----
+
+    # Display user controlled square
     filled_rect = pygame.Rect(game_square.pos_x, game_square.pos_y, 25, 25)
     pygame.draw.rect(game_window, (0, 0, 0), filled_rect)
 
+    # Display objective square
     empty_rect = pygame.Rect(objective_square.pos_x, objective_square.pos_y, 25, 25)
     pygame.draw.rect(game_window, (255, 0, 0), empty_rect, 3)
 
+    # Display scoreboard
     text = font.render("Score: " + str(score), True, BLACK)
     textRect = text.get_rect()
     textRect.center = (WINDOW_WIDTH // 2, 25)
-
     game_window.blit(text, textRect)
+
     # Update display
     pygame.display.update()
 
+# Exit gracefully
 pygame.quit()
 sys.exit()
